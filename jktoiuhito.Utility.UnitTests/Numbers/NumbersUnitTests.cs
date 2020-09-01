36f6086b1de43dfed1f234302de3d35f4d25515b
jktoiuhito <56585957+jktoiuhito@users.noreply.github.com>
2020-09-01 15:31:17 +0300

    version 3.2.1

---
+++ jktoiuhito.Utility.UnitTests/Numbers/NumbersUnitTests.cs
@@ -0,0 +1,384 @@
+﻿using jktoiuhito.Utility.Numbers;
+using Xunit;
+using System;
+
+/*
+ * This file was automatically generated 2020/09/01 12:19:08 UTC time.
+ * All manual changes will be lost.
+ */
+
+namespace jktoiuhito.Utility.UnitTests.Numbers
+{
+    public class NumbersUnitTests
+    {
+                    [Theory]
+            [InlineData(-1, "")]
+            [InlineData(-1, " ")]
+            [InlineData(-1, "　")]
+            [InlineData(-1, "	")]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void sbyteNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                sbyte number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+            
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void sbyteNotZero_Negative_ReturnsInput (string localname)
+            {
+                sbyte number = -1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void sbyteNotZero_Positive_ReturnsInput (string localname)
+            {
+                sbyte number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void sbyteNotZero_Zero_ThrowsException (string localname)
+            {
+                sbyte number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+                    [Theory]
+            [InlineData(-1, "")]
+            [InlineData(-1, " ")]
+            [InlineData(-1, "　")]
+            [InlineData(-1, "	")]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void shortNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                short number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+            
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void shortNotZero_Negative_ReturnsInput (string localname)
+            {
+                short number = -1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void shortNotZero_Positive_ReturnsInput (string localname)
+            {
+                short number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void shortNotZero_Zero_ThrowsException (string localname)
+            {
+                short number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+                    [Theory]
+            [InlineData(-1, "")]
+            [InlineData(-1, " ")]
+            [InlineData(-1, "　")]
+            [InlineData(-1, "	")]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void intNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                int number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+            
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void intNotZero_Negative_ReturnsInput (string localname)
+            {
+                int number = -1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void intNotZero_Positive_ReturnsInput (string localname)
+            {
+                int number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void intNotZero_Zero_ThrowsException (string localname)
+            {
+                int number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+                    [Theory]
+            [InlineData(-1, "")]
+            [InlineData(-1, " ")]
+            [InlineData(-1, "　")]
+            [InlineData(-1, "	")]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void longNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                long number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+            
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void longNotZero_Negative_ReturnsInput (string localname)
+            {
+                long number = -1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void longNotZero_Positive_ReturnsInput (string localname)
+            {
+                long number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void longNotZero_Zero_ThrowsException (string localname)
+            {
+                long number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+            
+                    [Theory]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void byteNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                byte number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void byteNotZero_Positive_ReturnsInput (string localname)
+            {
+                byte number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void byteNotZero_Zero_ThrowsException (string localname)
+            {
+                byte number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+                    [Theory]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void ushortNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                ushort number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void ushortNotZero_Positive_ReturnsInput (string localname)
+            {
+                ushort number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void ushortNotZero_Zero_ThrowsException (string localname)
+            {
+                ushort number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+                    [Theory]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void uintNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                uint number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void uintNotZero_Positive_ReturnsInput (string localname)
+            {
+                uint number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void uintNotZero_Zero_ThrowsException (string localname)
+            {
+                uint number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+                    [Theory]
+            [InlineData(0, "")]
+            [InlineData(0, " ")]
+            [InlineData(0, "　")]
+            [InlineData(0, "	")]
+            [InlineData(1, "")]
+            [InlineData(1, " ")]
+            [InlineData(1, "　")]
+            [InlineData(1, "	")]
+            public void ulongNotZero_EmptyWhitespaceLocalname_ThrowsException (
+                ulong number, string localname)
+            {
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void ulongNotZero_Positive_ReturnsInput (string localname)
+            {
+                ulong number = 1;
+    
+                var result = number.NotZero(localname);
+    
+                Assert.Equal(number, result);
+            }
+    
+            [Theory]
+            [InlineData(null)]
+            [InlineData("name")]
+            public void ulongNotZero_Zero_ThrowsException (string localname)
+            {
+                ulong number = 0;
+    
+                Assert.Throws<ArgumentException>(
+                    () => number.NotZero(localname));
+            }
+            }
+}
---
+++ jktoiuhito.Utility/Numbers/Numbers.cs
@@ -0,0 +1,185 @@
+﻿using jktoiuhito.Utility.String;
+using System;
+
+/*
+ * This file was automatically generated 2020/09/01 12:19:26 UTC time.
+ * All manual changes will be lost.
+ */
+
+namespace jktoiuhito.Utility.Numbers
+{
+    /// <summary>
+    /// Extension methods related to numbers.
+    /// </summary>
+    public static class Numbers
+    {
+            /// <summary>
+        /// Returns the input <see cref="sbyte"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="sbyte"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="sbyte"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static sbyte NotZero (this sbyte number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+            /// <summary>
+        /// Returns the input <see cref="short"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="short"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="short"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static short NotZero (this short number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+            /// <summary>
+        /// Returns the input <see cref="int"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="int"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="int"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static int NotZero (this int number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+            /// <summary>
+        /// Returns the input <see cref="long"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="long"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="long"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static long NotZero (this long number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+                /// <summary>
+        /// Returns the input <see cref="byte"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="byte"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="byte"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static byte NotZero (this byte number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+            /// <summary>
+        /// Returns the input <see cref="ushort"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="ushort"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="ushort"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static ushort NotZero (this ushort number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+            /// <summary>
+        /// Returns the input <see cref="uint"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="uint"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="uint"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static uint NotZero (this uint number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+            /// <summary>
+        /// Returns the input <see cref="ulong"/> if it is not zero.
+        /// Otherwise throws an exception.
+        /// </summary>
+        /// <param name="number">Input <see cref="ulong"/>.</param>
+        /// <param name="localname">Name of the variable in local context.</param>
+        /// <returns>Input <see cref="ulong"/></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="number"/> is zero.
+        ///     <paramref name="localname"/> is empty or whitespace.
+        /// </exception>
+        public static ulong NotZero (this ulong number, string localname = null)
+        {
+            if (localname != null)
+                localname = localname.NotNullEmptyWhitespace(nameof(localname));
+            if (number == 0)
+                throw localname == null
+                    ? new ArgumentException("number cannot be zero")
+                    : new ArgumentException("number cannot be zero", localname);
+            return number;
+        }
+        }
+}
